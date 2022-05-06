c88dd25efdf663952c6e163de3cf5a22264177b5
Mary White <mary.a.white28@gmail.com>
2022-05-06 11:19:48 -0400

    fixed typo in calculateAge function and added an alternative solution not using Date String

--- age.js
+++ age.js
@@ -2,7 +2,7 @@
 
 function calculateAge(dateString) {
     let today = new Date();
-    let myBday = new Date("1984-12-18");
+    let myBday = new Date(dateString);
     let age = today.getFullYear() - myBday.getFullYear();
     let mon = today.getMonth() - myBday.getMonth();
     //this if statement will account for the fact that your birthday may not have happened yet this year
@@ -11,7 +11,25 @@ function calculateAge(dateString) {
     }
     return age;
 }
-
-console.log(calculateAge("1984-12-18"));
+//the dateString should be in the format "yyyy-MM-dd"
+console.log(calculateAge("1984-12-15"));
+console.log(calculateAge("1948-12-15"));
 
 //to run this using node, use the terminal to navigate to the correct directory, then type node age.js
+
+//Alternative method, if you don't want to use a date String, you can use year, month, day as follows:
+function calculateAgeV2(y, m, d) {
+    let today = new Date();
+    let bDay = new Date(y, m, d)
+    let age = today.getFullYear() - bDay.getFullYear();
+    let mon = today.getMonth() - bDay.getMonth();
+    //this if statement will account for the fact that your birthday may not have happened yet this year
+    if(mon < 0 || (mon === 0 & today.getDate() < bDay.getDate())) {
+        age--;
+    }
+    return age;
+}
+//input should be in the format yyyy, MM, dd
+console.log("-------------------------");
+console.log(calculateAgeV2(1984, 12, 15));
+console.log(calculateAgeV2(1948, 12, 15));
